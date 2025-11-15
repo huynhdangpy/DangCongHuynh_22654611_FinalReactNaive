7a2ea53e24eb7f9067cb67b2de45cd17e607a7ca
H82M19\Student <Student>
2025-11-15 15:27:27 +0700

    init expo project and sqlite setup for simple contacts

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
+import { getDb } from "../src/db";
 
 export default function Index() {
+  useEffect(() => {
+    async function testDB() {
+      const db = await getDb();
+      console.log("SQLite connected:", db);
+    }
+    testDB();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Simple Contacts App - Câu 1 OK</Text>
     </View>
   );
 }
